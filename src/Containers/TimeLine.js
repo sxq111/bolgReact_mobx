fe4b555193761938faab6c29df2925ad82c9da11
宋锡铨 <1044955115@qq.com>
2018-03-24 23:50:13 +0800

    学习类代码

--- src/Containers/TimeLine.js
+++ src/Containers/TimeLine.js
@@ -1,16 +1,47 @@
 import React, { Component } from 'react';
 
 export default class extends Component {
-    componentDidMount(){
+    componentDidMount() {
         this.props.onTimeline();
-        setInterval(()=>{
-            console.log(this.props.timeLineData);
-        },1000)
+        // setInterval(()=>{
+        //     console.log(this.props.timeLineData);
+        // },1000)
     }
-    componentWillUnmount(){
+    componentWillUnmount() {
         this.props.onNormal();
     }
-    render(){
+    render() {
+        // console.log(
+        //     getNum_N([1, 1], (n, calculator) => {
+        //         if (n <= 2)
+        //             return 1;
+        //         return calculator(n - 1) + calculator(n - 2);
+        //     })(10)
+        // );
+        // console.log(
+        //     getNum_N([1], (n, calculator) => {
+        //         if (n <= 1)
+        //             return 1;
+        //         return calculator(n - 1) * n;
+        //     })(5)
+        // );
         return (<div></div>)
     }
 }
+// const getNum_N = function (knowedValues = [], logicFunc) {
+//     const calculator = (num) => {
+//         if (knowedValues[num] !== undefined) {
+//             return knowedValues[num];
+//         }
+//         knowedValues[num] = logicFunc(num, calculator);
+//         return knowedValues[num];
+//     }
+//     return calculator;
+// }
+
+// const badFib = (n) => {
+//     if (n <= 2) {
+//         return 1;
+//     }
+//     return badFib(n - 1) + badFib(n - 2);
+// }
