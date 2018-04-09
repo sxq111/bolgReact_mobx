bf65c3cd64f072080c62a1098ba606f7149b6a46
sxq111 <1044955115@qq.com>
2018-04-09 21:01:52 +0800

    更改样式

--- src/Components/Nav2.js
+++ src/Components/Nav2.js
@@ -4,13 +4,19 @@ import styles from './nav.css';
 {/* <Icon type="bars" /> */}
 @withRouter
 export default class extends Component {
+    clkNode(tag){
+        this.props.onChangeTag(tag);
+        this.props.history.push('/');
+    }
     render() {
         return(
             <div style={{ width: 100 }} className = {styles.navBody}>
                 {
                     Object.keys(this.props.fileMap || []).map(tag => {
                         return (
-                            <div className  = {styles.navItem}>{tag}</div>
+                            <div className  = {styles.navItem+' '+(this.props.tag === tag?styles.navItemSelected:'')}
+                            onClick = {this.clkNode.bind(this,tag)}
+                            >{tag}</div>
                         );
                     })
                 }
